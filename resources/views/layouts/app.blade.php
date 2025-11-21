45a79f4907cd9b14544eab70df1ed44206234243
rizkylab <rizky.sulistyo@unib.ac.id>
2025-11-22 00:07:27 +0700

    Add : SweetAlert integration for all CRUD notifications

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,9 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
+    <!-- SweetAlert2 -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body class="font-sans antialiased bg-gray-50 dark:bg-gray-900">
     <div class="min-h-screen flex">
@@ -79,22 +82,59 @@
 
             <!-- Page Content -->
             <main class="p-6">
-                <!-- Flash Messages -->
-                @if (session('success'))
-                    <div class="mb-4 p-4 bg-green-50 dark:bg-green-900/20 border border-green-200 dark:border-green-800 rounded-lg">
-                        <p class="text-green-800 dark:text-green-200">{{ session('success') }}</p>
-                    </div>
-                @endif
-
-                @if (session('error'))
-                    <div class="mb-4 p-4 bg-red-50 dark:bg-red-900/20 border border-red-200 dark:border-red-800 rounded-lg">
-                        <p class="text-red-800 dark:text-red-200">{{ session('error') }}</p>
-                    </div>
-                @endif
-
                 {{ $slot }}
             </main>
         </div>
     </div>
+
+    <!-- SweetAlert Flash Messages -->
+    <script>
+        // Auto-show SweetAlert for flash messages
+        @if (session('success'))
+            Swal.fire({
+                icon: 'success',
+                title: 'Success!',
+                text: '{{ session('success') }}',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                toast: true,
+                position: 'top-end'
+            });
+        @endif
+
+        @if (session('error'))
+            Swal.fire({
+                icon: 'error',
+                title: 'Error!',
+                text: '{{ session('error') }}',
+                showConfirmButton: true,
+                confirmButtonColor: '#EF4444'
+            });
+        @endif
+
+        @if (session('warning'))
+            Swal.fire({
+                icon: 'warning',
+                title: 'Warning!',
+                text: '{{ session('warning') }}',
+                showConfirmButton: true,
+                confirmButtonColor: '#F59E0B'
+            });
+        @endif
+
+        @if (session('info'))
+            Swal.fire({
+                icon: 'info',
+                title: 'Information',
+                text: '{{ session('info') }}',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                toast: true,
+                position: 'top-end'
+            });
+        @endif
+    </script>
 </body>
 </html>
